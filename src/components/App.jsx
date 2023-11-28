d8d969cc20246d3830140645bffb15f21797fc1a
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-11-28 22:13:03 +0200

    commit2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,32 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
-
 import FriendList from './FriendList';
 import Profile from './Profile';
 import Statistics from './Statistics';
 import TransactionHistory from './TransactionHistory';
 
-const App = () => {
+ const App = () => {
   return (
-    <>
+    <div
+      
+    >
       <Profile />
       <Statistics />
       <FriendList />
       <TransactionHistory />
-    </>
+    </div>
   );
 };
+
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <Profile />
+//       <Statistics />
+//       <FriendList />
+//       <TransactionHistory />
+//     </>
+//   );
+// };
 
 export default App;
